4a619242bc904e1f4d317773de6e5fcafd3208f0
cbrwizard <cbrwizard@gmail.com>
2016-11-03 23:41:03 +0300

    Game scene
    Adds a game scene.
    Adds a SceneSwitcher script.
    Switches to a game scene on New game click in menu.

---
+++ Elon Goes To Mars/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+  public void SwitchScene(string sceneName)
+  {
+    SceneManager.LoadScene(sceneName);
+    Scene scene = SceneManager.GetSceneByName(sceneName);
+
+    SceneManager.SetActiveScene(scene);
+  }
+}
