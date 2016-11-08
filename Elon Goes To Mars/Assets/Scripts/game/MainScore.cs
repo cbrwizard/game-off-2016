de8f8dfab73c1558f1d75114bda2b4c9acf4f9eb
cbrwizard <cbrwizard@gmail.com>
2016-11-08 22:38:04 +0300

    Score
    Adds a score label and a method to update it.

---
+++ Elon Goes To Mars/Assets/Scripts/game/MainScore.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/**
+  Stores the Score value and dispatches an event on it's change.
+**/
+public class MainScore : MonoBehaviour {
+  private int _score = 1000;
+
+  public int score
+  {
+    get {
+      return _score;
+    }
+    set
+    {
+      if (value < 0) {
+        _score = 0;
+      } else {
+        _score = value;
+      }
+
+      SendMessage("HandleScoreChange", _score);
+    }
+  }
+}
---
+++ Elon Goes To Mars/Assets/Scripts/ui_controllers/ScoreController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/**
+  Binds the text field to a Score. Updates it when changed.
+**/
+public class ScoreController : MonoBehaviour {
+  public Text scoreText;
+  public MainScore mainScoreScript;
+
+  void Start()
+  {
+    updateScoreText(mainScoreScript.score);
+  }
+
+  public void HandleScoreChange(int newScore)
+  {
+    updateScoreText(newScore);
+  }
+
+  private void updateScoreText(int newScore)
+  {
+    scoreText.text = newScore.ToString();
+  }
+}
