7468863e3571928e51d97bbb049c1ed0c89e7a33
cbrwizard <cbrwizard@gmail.com>
2016-11-23 23:26:51 +0300

    Fade in text on death

---
+++ Elon Goes To Mars/Assets/FadeIn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/**
+  Is responsible for fading in an object.
+**/
+public class FadeIn : MonoBehaviour {
+  public Graphic graphicToFade;
+  public float speed;
+
+  public void Run()
+  {
+    graphicToFade.CrossFadeAlpha(250, speed, true);
+  }
+}
