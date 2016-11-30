4c3df77a198c90acaf7fd755e44b9be9f67cb2e5
cbrwizard <cbrwizard@gmail.com>
2016-12-01 01:54:35 +0300

    Intro!
    Adds a video at start.

---
+++ Elon Goes To Mars/Assets/Scripts/lib/MovieStarter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/**
+  Is responsible for starting a movie.
+  TODO: make it invoke a finish event; add a controller and invoke a scene switch somewhere.
+**/
+public class MovieStarter : MonoBehaviour {
+
+  public string sceneToSwitch;
+  private MovieTexture movieTexture;
+  private SceneSwitcher sceneSwitcher;
+
+  void Start () {
+    movieTexture = ((MovieTexture)GetComponent<Renderer>().material.mainTexture);
+    sceneSwitcher = GetComponent<SceneSwitcher>();
+    movieTexture.Play();
+  }
+
+  void Update () {
+    if(!movieTexture.isPlaying){
+      sceneSwitcher.SwitchScene(sceneToSwitch);
+    }
+  }
+}
