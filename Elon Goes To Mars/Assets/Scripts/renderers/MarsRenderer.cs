0fe9c0f7f47001150bbb391652fcc55f563d518c
cbrwizard <cbrwizard@gmail.com>
2016-11-30 22:51:05 +0300

    Victory scene
    Adds a basic victory scene.
    Switches to it on Win.

--- Elon Goes To Mars/Assets/Scripts/renderers/MarsRenderer.cs
+++ Elon Goes To Mars/Assets/Scripts/renderers/MarsRenderer.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using System.Collections;
 
 /**
   Is responsible for rendering Mars and moving it to a Player.
+  TODO: check if Update is called when not rendered (prob not)
+  TODO: move the scene switch to another class or trigger an event.
 **/
 public class MarsRenderer : MonoBehaviour {
   public GameObject marsPrefab;
@@ -12,10 +15,20 @@ public class MarsRenderer : MonoBehaviour {
 
   private bool movingLeft = false;
   private GameObject mars;
+  private Vector3 target;
+  private GameObject mainController;
+  private SceneSwitcher sceneSwitcher;
 
   public MarsRenderer(GameObject passedMarsPrefab)
   {
     marsPrefab = passedMarsPrefab;
+  }
+
+  void Start()
+  {
+    target = new Vector3(finishX, startingY, 0);
+    mainController = GameObject.FindWithTag("MainController");
+    sceneSwitcher = mainController.GetComponent<SceneSwitcher>();
   }
 
   public void HandleWin()
@@ -43,8 +56,20 @@ public class MarsRenderer : MonoBehaviour {
     {
       float step = speed * Time.unscaledDeltaTime;
       mars.transform.position = Vector3.MoveTowards(
-        mars.transform.position, new Vector3(finishX, startingY, 0), step
+        mars.transform.position, target, step
       );
+
+      if(Vector3.Distance(mars.transform.position, target) < 0.1f)
+      {
+        movingLeft = false;
+        StartCoroutine(LoadVictoryScene());
+      }
     }
+  }
+
+  IEnumerator LoadVictoryScene()
+  {
+    yield return new WaitForSecondsRealtime(1.5f);
+    sceneSwitcher.SwitchScene("Victory");
   }
 }
